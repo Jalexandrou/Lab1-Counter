c37ed4602113e78393478669cc48db614b66ca10
Jalexandrou <jalexandrou10@gmail.com>
2022-10-26 19:52:34 +0100

    Completed task 3

---
+++ task3/counter_tb LOADABLE.cpp
@@ -0,0 +1,61 @@
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env) {
+    int i;
+    int clk;
+
+Verilated::commandArgs(argc, argv);
+// init top verilog instance
+Vcounter* top = new Vcounter;
+//init trace dump
+Verilated::traceEverOn(true);
+VerilatedVcdC* tfp = new VerilatedVcdC;
+top->trace (tfp, 99);
+tfp->open ("counter.vcd");
+
+//init Vbuddy
+if (vbdOpen()!=1) return(-1);
+vbdHeader("Lab 1: Counter");
+vbdSetMode(1);
+
+//initialize simulation inputs
+top->clk = 1;
+top->rst = 1;
+top->en = 0;
+
+//run simulation for many clock cycles 
+for (i=0; i<500; i++){
+
+    //dump variables into VCD file and toggle clock
+    for(clk=0; clk<2; clk++) {
+        tfp->dump (2*i+clk);
+        top->clk = !top->clk;
+        top->eval ();
+    }
+
+    //Code for loadable counter, changes the counter to Vbuddy’s stored parameter value.
+    if(vbdFlag()){
+        (top->count) = vbdValue();
+    }  
+
+    //++++ Send count value to Vbuddy
+    vbdHex(4, (int(top->count) >> 16) & 0xF);
+    vbdHex(3, (int(top->count) >> 8) & 0xF);
+    vbdHex(2, (int(top->count) >> 4) & 0xF);
+    vbdHex(1, int(top->count) & 0xF);
+    vbdCycle(i+1);
+    //---- end of Vbuddy output section
+
+    //change input stimuli
+    top->rst = (i<2) | (i == 0);
+    top->en = (i>4);
+    if (Verilated::gotFinish()) exit(0);
+}
+
+vbdClose();
+tfp->close();
+exit(0);
+}
